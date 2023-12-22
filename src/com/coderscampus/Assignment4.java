583b03d04ca15c7a9b27fc2be88de33162aebdf3
KeatonCable1 <keatoncable1@gmail.com>
2023-12-22 18:28:06 -0500

    Assignment4

--- src/com/coderscampus/Assignment4.java
+++ src/com/coderscampus/Assignment4.java
@@ -3,8 +3,10 @@ package com.coderscampus;
 public class Assignment4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] lines = readStudentMaster("studentmasterlist");
 
 	}
 
+	
 }
